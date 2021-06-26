901fe3a9be1fe7ae2643883a56f7f14f88a0a998
daniel.cabral <daniel.cabral@thesunexchange.com>
2021-06-26 20:53:29 +0200

    minimize use of module dots

--- src/IO/Subcreator.hs
+++ src/IO/Subcreator.hs
@@ -10,11 +10,11 @@ import qualified Data.Set as S
 import Data.Maybe
 import Foreign.Ptr
 
-import IO.Raylib
+import IO.Raylib as Raylib
 
 import Core.Math
 import Core.Auditory
-import Core.Tactile
+import Core.Tactile as Tactile
 import Core.Visual
 import Core.Universe
 
@@ -41,7 +41,7 @@ subcreate u = do (u', am) <- begin u
 --begin world, init, load and setup initial state
 begin :: Universe a -> IO (Universe a, ArtMap)
 begin u = do    let r = uResolution u
-                r' <- IO.Subcreator.initWindow (uName u) r supportedResolutions
+                r' <- initDisplay (uName u) r supportedResolutions
                 initAudioDevice
                 sss <- loadSpriteSheets (uSpriteSheets u)
                 ss <- loadSounds (uSounds u)
@@ -49,26 +49,26 @@ begin u = do    let r = uResolution u
                 fs <- loadFonts (uFonts u)
                 toggleFullScreen
                 setTargetFPS (uFps u)
-                let u' = u { uScaleFactor = Core.Visual.scaleFactor r r' }
+                let u' = u { uScaleFactor = scaleFactor r r' }
                 return (u',  (sss, fs, ss, ms))
 
---init window with best resolution
-initWindow :: String -> Resolution -> [Resolution] -> IO Resolution
-initWindow t o rs = do IO.Raylib.initWindow (fst o) (snd o) t
-                       displayWidth <- getMonitorWidth 0
-                       displayHeight <- getMonitorHeight 0
-                       traceLog Info $ "ORIGINAL RESOLUTION " ++ show o
-                       traceLog Info $ "This ROCKS"
-                       traceLog Info $ "CURRENT DISPLAY " ++ show (displayWidth, displayHeight)
-                       r <- if o == (displayWidth, displayHeight)
-                            then do  traceLog Info $ "USE Original resolution ?" ++ show o
-                                     return o
-                            else do  closeWindow
-                                     let r = selectResolution (displayWidth, displayHeight) rs
-                                     traceLog Info $ "SELECT best resolution " ++ show r
-                                     IO.Raylib.initWindow (fst r) (snd r) t
-                                     return r
-                       return r
+--init display with best resolution
+initDisplay :: String -> Resolution -> [Resolution] -> IO Resolution
+initDisplay t o rs = do initWindow (fst o) (snd o) t
+                        displayWidth <- getMonitorWidth 0
+                        displayHeight <- getMonitorHeight 0
+                        traceLog Info $ "ORIGINAL RESOLUTION " ++ show o
+                        traceLog Info $ "This ROCKS"
+                        traceLog Info $ "CURRENT DISPLAY " ++ show (displayWidth, displayHeight)
+                        r <- if o == (displayWidth, displayHeight)
+                             then do  traceLog Info $ "USE Original resolution ?" ++ show o
+                                      return o
+                             else do  closeWindow
+                                      let r = selectResolution (displayWidth, displayHeight) rs
+                                      traceLog Info $ "SELECT best resolution " ++ show r
+                                      initWindow (fst r) (snd r) t
+                                      return r
+                        return r
 
 --main loop for existing world
 exist :: Universe a -> ArtMap -> IO ()
@@ -124,36 +124,36 @@ visuals u tm fm  =  do beginDrawing
                        where sf = uScaleFactor u
 
 drawSprite :: Sprite -> TextureMap -> (ScaleFactor, ScaleFactor) -> IO ()
-drawSprite s tm sf = do drawTexturePro a sr dr (IO.Raylib.Vector2 0 0) 0.0 white
+drawSprite s tm sf = do drawTexturePro a sr dr (Vector2 0 0) 0.0 white
                         where d = dimensions s
                               sr = toRectangle (sourcePosition s) d (1.0, 1.0)
                               dr = toRectangle (targetPosition s) d sf
                               a = findArt tm (spriteSheet s)
 --tactiles
 tactiles :: IO Tactile
-tactiles = do let ks = zip keyboardKeys [Core.Tactile.Key_Apostrophe .. Core.Tactile.Key_Kp_Equal]
+tactiles = do let ks = zip keyboardKeys [Tactile.Key_Apostrophe .. Tactile.Key_Kp_Equal]
               ksp <- keysTouched ks isKeyPressed
               ksd <- keysTouched ks isKeyDown
-              return $ Tactile { Core.Tactile.keysPressed = ksp, Core.Tactile.keysDown = ksd }
+              return $ Tactile { keysPressed = ksp, keysDown = ksd }
 
-keysTouched :: [(IO.Raylib.KeyboardKey, Core.Tactile.KeyboardKey)] -> (IO.Raylib.KeyboardKey -> IO Bool) -> IO (Set Core.Tactile.KeyboardKey)
+keysTouched :: [(Raylib.KeyboardKey, Tactile.KeyboardKey)] -> (Raylib.KeyboardKey -> IO Bool) -> IO (Set Tactile.KeyboardKey)
 keysTouched xs f = do ks <- mapM (\(x, y) -> do kp <- f x
-                                                return (if kp then y else Core.Tactile.Key_Null)) xs
+                                                return (if kp then y else Tactile.Key_Null)) xs
                       return (S.fromList ks)
 
 findArt :: Map String (Ptr a) -> String -> Ptr a
 findArt m k = fromJust (M.lookup k m)
 
 --conversions
-toRectangle :: Position -> Dimensions -> (ScaleFactor, ScaleFactor) -> IO.Raylib.Rectangle
-toRectangle p (w, h) (s1, s2) = IO.Raylib.Rectangle (realToFrac x') (realToFrac y') (realToFrac w') (realToFrac h')
+toRectangle :: Position -> Dimensions -> (ScaleFactor, ScaleFactor) -> Rectangle
+toRectangle p (w, h) (s1, s2) = Rectangle (realToFrac x') (realToFrac y') (realToFrac w') (realToFrac h')
                                      where (w', h') = scaleDimensions (w, h) (s1, s2)
                                            (x', y') = translatePosition p (s1, s2)
 
 --font
 drawText :: Ptr Font -> String -> Position -> ScaleFactor -> IO ()
 drawText f s p sf = do bs <- baseSize f
-                       drawTextEx f s (IO.Raylib.Vector2 (fromIntegral (x p)) (fromIntegral (y p))) (fromIntegral bs * sf) sf white
+                       drawTextEx f s (Vector2 (fromIntegral (x p)) (fromIntegral (y p))) (fromIntegral bs * sf) sf white
 
 
 --adapt positions, dimensions
